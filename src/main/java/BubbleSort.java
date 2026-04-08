7d31d6b6c00a4903cc9b2c1d465186a33aea6d4a
dr-roomba <feharlow123@gmail.com>
2026-04-08 15:12:09 +0100

    fixed forgotten class wrappers

--- src/main/java/BubbleSort.java
+++ src/main/java/BubbleSort.java
@@ -1,10 +1,12 @@
-public void BubbleSort(int[] arr){
-    for(int i = 0; i < arr.length - 1; i++){
-        for(int j = 0; j < arr.length - 2 - i; j++){
-            if(arr[j] > arr[j + 1]){
-                int temp = arr[j];
-                arr[j] = arr[j + 1];
-                arr[j + 1] = temp;
+public class BubbleSort {
+    public static void BubbleSort(int[] arr) {
+        for (int i = 0; i < arr.length - 1; i++) {
+            for (int j = 0; j < arr.length - 2 - i; j++) {
+                if (arr[j] > arr[j + 1]) {
+                    int temp = arr[j];
+                    arr[j] = arr[j + 1];
+                    arr[j + 1] = temp;
+                }
             }
         }
     }
