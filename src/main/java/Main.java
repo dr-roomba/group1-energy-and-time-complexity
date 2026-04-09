ac92db60a0aed9782751f20be0018e4c69957020
dr-roomba <feharlow123@gmail.com>
2026-04-09 10:36:53 +0100

    debug counting sort, the third

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -2,7 +2,7 @@ public class Main {
 
     public static void main(String[] args) {
 
-        int[] ints = {2, 6, 1, 8 ,4, 3, 5, 10, 7, 9};
+        int[] ints = {2, 6, 1, 8, 4, 3, 5, 10, 7, 9};
         int[] out = new int[10];
 
         CountingSort.CountingSort(ints, out, 10);
