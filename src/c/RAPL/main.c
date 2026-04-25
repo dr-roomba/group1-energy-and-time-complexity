2db9a8e401be21e7147205550719479ba613b21b
MR Osama <93708793+OsamaKharita@users.noreply.github.com>
2026-04-25 20:58:00 +0100

    Comments Deleted

--- src/c/RAPL/main.c
+++ src/c/RAPL/main.c
@@ -15,7 +15,7 @@ int main (int argc, char **argv)
   int  i=0;
 
 #ifdef RUNTIME
-  //clock_t begin, end;
+
   double time_spent;
   struct timeval tvb,tva;
 #endif
@@ -23,7 +23,6 @@ int main (int argc, char **argv)
   FILE * fp;
 
   //Run command
-//  strcpy(command, "./" );
   strcat(command,argv[1]);
   //Language name
   strcpy(path,"../");
@@ -35,14 +34,14 @@ int main (int argc, char **argv)
   //Test name
   strcpy(test,argv[3]);
 
-  //ntimes = atoi (argv[2]);
+
  
 
   fp = fopen(path,"a");
 
   rapl_init(core);
 
-  //fprintf(fp,"Package , CPU , GPU , DRAM? , Time (sec) \n");
+
 
   double avgEnergy = 0;
   double avgTime = 0;
@@ -53,11 +52,11 @@ int main (int argc, char **argv)
     {  
  
       fprintf(stdout, "Round: %d\n", i);
-    	// fprintf(fp,"%s ; ",test);
+
  	
 	      
 		#ifdef RUNTIME
-		        //begin = clock();
+
 				gettimeofday(&tvb,0);
 		#endif
 	
@@ -68,8 +67,7 @@ int main (int argc, char **argv)
 	energy = rapl_after(fp,core);
 
 		#ifdef RUNTIME
-			//end = clock();
-			//time_spent = (double)(end - begin) / CLOCKS_PER_SEC;
+
 			gettimeofday(&tva,0);
 			time_spent = (tva.tv_sec-tvb.tv_sec)*1000000 + tva.tv_usec-tvb.tv_usec;
 			time_spent = time_spent / 1000;
@@ -79,7 +77,7 @@ int main (int argc, char **argv)
 			
 
 		#ifdef RUNTIME	
-			// fprintf(fp," %G \n",time_spent);
+
 		#endif	
     }
     avgEnergy /= ntimes;
